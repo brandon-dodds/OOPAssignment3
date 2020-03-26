2273ad17e76d9448894f77b6d6aa8fd671d5f17a
Brandon-Lee Dodds <brandon.lee.dodds@gmail.com>
2020-03-26 18:27:01 +0000

    Thinking about Line Pointers...

--- OOPAssignment3/Diff.cs
+++ OOPAssignment3/Diff.cs
@@ -1,50 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 namespace OOPAssignment3
 {
     class Diff : ICommandable
     {
-        string[] Args { get; set; }
+        private string[] Args { get; set; }
+        private string[] formerFile { get; set; }
+        private string[] latterFile { get; set; }
+        
+        private void DiffLines()
+        {
+            string[] formerLinePointer = default;
+            string[] latterLinePointer = default;
+        }
         public void Help() => Console.WriteLine("Please enter: diff [text1] [text2]");
-        private int[] DiffLines()
-        {
-            FileReader file1 = new FileReader(Args[1]);
-            FileReader file2 = new FileReader(Args[2]);
-            List<int> changedLineNumbers = new List<int>();
-            int smallerFileSize;
-            int largerFileSize;
-            if (file1.fileContents.Length > file2.fileContents.Length)
-            {
-                smallerFileSize = file2.fileContents.Length;
-                largerFileSize = file1.fileContents.Length;
-            }
-            else
-            {
-                smallerFileSize = file1.fileContents.Length;
-                largerFileSize = file2.fileContents.Length;
-            }
-
-            for (int i = 0; i < smallerFileSize; i++)
-            {
-                if (file1.fileContents[i] != file2.fileContents[i])
-                {
-                    Console.WriteLine($"Line {i + 1} is different.");
-                    changedLineNumbers.Add(i);
-                }
-            }
-
-            for (int i = smallerFileSize; i < largerFileSize; i++)
-            {
-                Console.WriteLine($"Line {i + 1} is different.");
-                changedLineNumbers.Add(i);
-            }
-
-            return changedLineNumbers.ToArray();
-        }
-        private void DiffChars(int[] changedLines)
-        {
-
-        }
         public void Run()
         {
             if (Args.Length == 1)
@@ -53,9 +21,9 @@ namespace OOPAssignment3
                 Help();
             else if (Args.Length == 3)
             {
-                int[] changedLines = DiffLines();
-                DiffChars(changedLines);
-
+                formerFile = new FileReader(Args[1]).fileContents;
+                latterFile = new FileReader(Args[2]).fileContents;
+                DiffLines();
             }
             else
                 Console.WriteLine("Diff requires exactly two arguments!");
