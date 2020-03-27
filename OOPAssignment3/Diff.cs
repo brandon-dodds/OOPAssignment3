e85d99598bcfa054bca0f5b3b0b9fa7de5fb072b
Brandon-Lee Dodds <brandon.lee.dodds@gmail.com>
2020-03-27 13:50:01 +0000

    Added comments. Made the char comparison.

--- OOPAssignment3/Diff.cs
+++ OOPAssignment3/Diff.cs
@@ -4,13 +4,89 @@ namespace OOPAssignment3
     class Diff : ICommandable
     {
         private string[] Args { get; set; }
-        private string[] formerFile { get; set; }
-        private string[] latterFile { get; set; }
+        private string[] FormerFile { get; set; }
+        private string[] LatterFile { get; set; }
         
         private void DiffLines()
         {
-            string[] formerLinePointer = default;
-            string[] latterLinePointer = default;
+            // Determines the larger file size.
+            int largerFileSize = FormerFile.Length;
+            if (FormerFile.Length < LatterFile.Length)
+                largerFileSize = LatterFile.Length;
+
+            for (int i = 0; i < largerFileSize; i++)
+            {
+                // Starts with Line 0 up to the larger file size length.
+                Console.WriteLine($"Line {i}");
+                /* Two line pointers are created.
+                 * They are null by default.
+                 * If the index is in range for the file, the pointer becomes that line.
+                 */
+                string formerLinePointer = null;
+                if (i < FormerFile.Length)
+                    formerLinePointer = FormerFile[i];
+                string latterLinePointer = null;
+                if (i < LatterFile.Length)
+                    latterLinePointer = LatterFile[i];
+
+                /* A char array is created.
+                 * if the line pointers are not null, then the char array = the specific line.
+                 */
+                char[] formerCharArray = null;
+                char[] latterCharArray = null;
+                if (formerLinePointer != null)
+                    formerCharArray = formerLinePointer.ToCharArray();
+                if (latterLinePointer != null)
+                    latterCharArray = latterLinePointer.ToCharArray();
+                
+                //if both the char arrays are not null (they have text)
+                if (formerCharArray != null && latterCharArray != null)
+                {
+                    // Figure out the larget line size. (character lengths).
+                    int largerLineSize = formerCharArray.Length;
+                    if (formerCharArray.Length < latterCharArray.Length)
+                        largerLineSize = latterCharArray.Length;
+
+                    for (int j = 0; j < largerLineSize; j++)
+                    {
+                        /* Create the specific char.
+                         * if the char index is in range, assign it to the indexed value of the char array
+                         */
+                        char formerCharPointer = default;
+                        if (j < formerCharArray.Length)
+                            formerCharPointer = formerCharArray[j];
+                        char latterCharPointer = default;
+                        if (j < latterCharArray.Length)
+                            latterCharPointer = latterCharArray[j];
+                        // If the chars are equal, then print normally.
+                        if(formerCharPointer == latterCharPointer)
+                            Console.Write(formerCharPointer);
+                        // If the former char pointer is not null, and the latter is null, show it as removed.
+                        else if (formerCharPointer != '\0' && latterCharPointer == '\0')
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write(formerCharPointer);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(latterCharPointer);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                if (formerCharArray == null && latterCharArray != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(latterCharArray);
+                }
+                if (formerCharArray != null && latterCharArray == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(formerCharArray);
+                }
+                Console.ResetColor();
+            }
         }
         public void Help() => Console.WriteLine("Please enter: diff [text1] [text2]");
         public void Run()
@@ -21,9 +97,13 @@ namespace OOPAssignment3
                 Help();
             else if (Args.Length == 3)
             {
-                formerFile = new FileReader(Args[1]).fileContents;
-                latterFile = new FileReader(Args[2]).fileContents;
-                DiffLines();
+                Console.ResetColor();
+                FormerFile = new FileReader(Args[1]).fileContents;
+                LatterFile = new FileReader(Args[2]).fileContents;
+                if(FormerFile == null || LatterFile == null)
+                    Console.WriteLine("The files entered need to exist!");
+                else
+                    DiffLines();
             }
             else
                 Console.WriteLine("Diff requires exactly two arguments!");
